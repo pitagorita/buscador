877f5f5008633e7db41277c63806748d42d12d3a
Pitagorita <66530757+pitagorita@users.noreply.github.com>
2024-08-23 15:51:18 -0400

    Archivo JS buscar

---
+++ buscar.js
@@ -0,0 +1,19 @@
+document.getElementById('query').addEventListener('input', function() {
+    const query = this.value;
+    fetch(`http://127.0.0.1:5000/buscar?q=${encodeURIComponent(query)}`)
+        .then(response => response.json())
+        .then(data => {
+            let resultsDiv = document.getElementById('results');
+            resultsDiv.innerHTML = '';
+
+            if (data.length > 0) {
+                data.forEach(item => {
+                    let p = document.createElement('p');
+                    p.textContent = JSON.stringify(item);
+                    resultsDiv.appendChild(p);
+                });
+            } else {
+                resultsDiv.textContent = 'No se encontraron resultados';
+            }
+        });
+});
