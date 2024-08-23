4b847d4801d154f7f285342dc46726b5f8c7944b
Pitagorita <66530757+pitagorita@users.noreply.github.com>
2024-08-23 16:44:08 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,22 +1,21 @@
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
+CORS(app)
 
-# Cargar la tabla de Excel
 df = pd.read_excel('datos_trabajadores.xlsx')
 
-# Ruta para la página principal
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
-
-# Ruta para el buscador
 @app.route('/buscar', methods=['GET'])
 def buscar():
     query = request.args.get('q', '').lower()
     resultados = df[df.apply(lambda row: row.astype(str).str.lower().str.contains(query).any(), axis=1)]
     return jsonify(resultados.to_dict(orient='records'))
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
