bdab75b6c362c46627879c6bf3686332802848a9
Pitagorita <66530757+pitagorita@users.noreply.github.com>
2024-08-23 16:02:46 -0400

    Update app.py agregada ruta static

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
-# Cargar la tabla de Excel al iniciar la app
+# Cargar la tabla de Excel
 df = pd.read_excel('datos_trabajadores.xlsx')
 
+# Ruta para la página principal
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# Ruta para el buscador
 @app.route('/buscar', methods=['GET'])
 def buscar():
-    try:
-        query = request.args.get('q', '').lower()
-        resultados = df[df.apply(lambda row: row.astype(str).str.lower().str.contains(query).any(), axis=1)]
-        return jsonify(resultados.to_dict(orient='records'))
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+    query = request.args.get('q', '').lower()
+    resultados = df[df.apply(lambda row: row.astype(str).str.lower().str.contains(query).any(), axis=1)]
+    return jsonify(resultados.to_dict(orient='records'))
 
 if __name__ == '__main__':
     app.run(debug=True)
